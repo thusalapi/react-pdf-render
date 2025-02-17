1e3d863c0927c866227bbeeb18d7041b28a1c5bb
Thusala Piyarisi <49372490+thusalapi@users.noreply.github.com>
2025-02-17 12:06:59 +0530

    fix build error

--- components/FieldPalette.tsx
+++ components/FieldPalette.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import SignatureField from "./SignatureField";
 
 interface FieldPaletteProps {
-  onFieldSelected: (fieldType: "signature" | "stamp" | null) => void; // optional
+  onFieldSelected: (fieldType: "signature" | "stamp" | null) => void;
 }
 
 const FieldPalette: React.FC<FieldPaletteProps> = ({ onFieldSelected }) => {
   return (
     <div style={{ marginBottom: "20px" }}>
-      <SignatureField id="signature-field" fieldType="signature" />
-      <SignatureField id="stamp-field" fieldType="stamp" />
+      <div onClick={() => onFieldSelected("signature")}>
+        <SignatureField id="signature-field" fieldType="signature" />
+      </div>
+      <div onClick={() => onFieldSelected("stamp")}>
+        <SignatureField id="stamp-field" fieldType="stamp" />
+      </div>
     </div>
   );
 };
